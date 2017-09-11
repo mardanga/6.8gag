bfa61a26e0047d637a8a25edf460d7bf752d56bc
mardanga <gaitan@CESVIARG>
2017-09-11 14:57:49 -0300

    pantallas y modal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
+import { SubirPage } from './../subir/subir';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {ModalController, NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private modalCrl: ModalController) {
 
+  }
+
+  postear() {
+    let modal = this.modalCrl.create(SubirPage) ;
+    modal.present();
+    
   }
 
 }
--- src/pages/subir/subir.ts
+++ src/pages/subir/subir.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-subir',
@@ -7,8 +7,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SubirPage {
 
-  constructor() {
+  constructor( private viewCtrl: ViewController ) {
   }
 
+  cerrar(){
+    this.viewCtrl.dismiss();
+  }
 
 }
